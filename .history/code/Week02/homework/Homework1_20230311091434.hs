26abad04394c1f2ad158e0f8a113d4ee7faac4f0
sergi <s@s.com>
2023-03-11 10:30:13 +0100

    Week02 homework1 done

---
+++ .history/code/Week02/homework/Homework1_20230311091434.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE TemplateHaskell     #-}
+
+module Homework1 where
+
+import qualified Plutus.V2.Ledger.Api as PlutusV2
+import           PlutusTx             (compile)
+import           PlutusTx.Prelude     (otherwise,Bool (..), BuiltinData, fst, snd, (==))
+import           Utilities            (wrap)
+
+---------------------------------------------------------------------------------------------------
+----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
+
+{-# INLINABLE mkValidator #-}
+-- This should validate if and only if the two Booleans in the redeemer are True!
+mkValidator :: () -> (Bool, Bool) -> PlutusV2.ScriptContext -> Bool
+mkValidator _ a _ gity 
+    | fst a == False    = False
+    | fst a == snd a    = True
+    -- | otherwise         = False
+
+wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+wrappedVal = wrap mkValidator
+{-# INLINABLE wrappedVal #-}
+
+validator :: PlutusV2.Validator
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| wrappedVal ||])
