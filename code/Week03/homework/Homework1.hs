53a3000e3566ee08d8735ef0c7332fcec99aa147
sergi <s@s.com>
2023-03-14 08:53:16 +0100

    WIP week03 homework1

--- code/Week03/homework/Homework1.hs
+++ code/Week03/homework/Homework1.hs
@@ -9,9 +9,10 @@ module Homework1 where
 
 import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
                                        ScriptContext, Validator,
+                                       TxInfo (txInfoValidRange, txInfoSignatories), to,
                                        mkValidatorScript)
 import           PlutusTx             (compile, unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool (..))
+import           PlutusTx.Prelude     (traceIfFalse, Bool (..), elem)
 import           Utilities            (wrap)
 
 ---------------------------------------------------------------------------------------------------
@@ -29,7 +30,38 @@ unstableMakeIsData ''VestingDatum
 -- This should validate if either beneficiary1 has signed the transaction and the current slot is before or at the deadline
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkVestingValidator :: VestingDatum -> () -> ScriptContext -> Bool
-mkVestingValidator _dat () _ctx = False -- FIX ME!
+mkVestingValidator _dat () _ctx = 
+    traceIfFalse "beneficiary's signature missing" checkBeneficiaryAndTime
+    -- traceIfFalse "beneficiary's signature missing" checkSig      &&
+    -- traceIfFalse "deadline not reached"            checkDeadline
+    where
+        txinf :: txInfo
+        txinf = scriptContextTxInfo _ctx
+
+        checkDeadline :: Bool
+        checkDeadline = to (deadline _dat) `contains` txInfoValidRange info 
+
+        isBeneficiary1 :: Bool
+        isBeneficiary1 = beneficiary1 _dat `elem` txInfoSignatories txinf
+
+        isBeneficiary2 :: Bool
+        isBeneficiary2 = beneficiary2 _dat `elem` txInfoSignatories txinf
+
+        checkBeneficiaryAndTime :: Bool
+        checkBeneficiaryAndTime = 
+            case checkDeadline of
+                True    -> isBeneficiary1
+                False   -> isBeneficiary2
+                
+            
+
+    
+    deadlineReached :: Bool
+   deadlineReached = contains (from $ deadline dat) $ txInfoValidRange info
+    
+    signer      = frst txInfoSignatories txinf
+    valid_range = txInfoValidRange txinf 
+    member 30 $ from (30 :: Integer)
 
 {-# INLINABLE  mkWrappedVestingValidator #-}
 mkWrappedVestingValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
